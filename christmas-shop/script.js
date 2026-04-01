8d3d0c486597d7dbe05bc7a52873abf227816bf6
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-04-01 21:28:53 +0300

    feat: redirect added to js class

---
+++ christmas-shop/script.js
@@ -0,0 +1,6 @@
+class RedirectHelper {
+    static redirect() {
+        const REDIRECT_URL = `${window.location.origin}/pavelhalanin-JSFEPRESCHOOL2026Q1/christmas-shop/home/`;
+        window.location.replace(REDIRECT_URL);
+    }
+}
